c6b6793afbe30d2d37b19d054c8ea9de3dd5e510
Louis Jarasius <louis@jarasi.us>
2025-08-30 17:00:58 +1000

    two-cols-header with less padding

---
+++ theme/layouts/two-cols-header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="slidev-layout two-cols-header w-full h-full">
+    <div class="grid grid-cols-1 grid-rows-[min-content_1fr] h-full gap-4">
+      <div class="header-section">
+        <slot />
+      </div>
+      <div class="grid grid-cols-2 gap-x-12 flex-1">
+        <div class="col-left">
+          <slot name="left" />
+        </div>
+        <div class="col-right">
+          <slot name="right" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
